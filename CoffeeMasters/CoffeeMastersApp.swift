e79af98396cdb932b57b004efa2e67b46689c3b8
Qiushi Yan <qiushi.yann@gmail.com>
2022-11-11 15:35:18 -0600

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by qiushi on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
